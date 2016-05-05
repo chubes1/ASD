17b3d7bfd689f4bb917df994a98f18cd385d6991
Connor Ciupek <connorciupek@me.com>
2016-05-05 16:04:57 +0100

    Removed bitcrush due to current inability to change, included working Convolution reverb

--- js/sampler.js
+++ js/sampler.js
@@ -1,18 +1,12 @@
 var context = new (window.AudioContext || window.webkitAudioContext)(),
     source,
-    impulseSource,
-    convolver = context.createConvolver();
-console.log(convolver);
-    var wetGain = context.createGain(),
-        dryGain = context.createGain(),
-        fileReader = new FileReader(),
-        audio,
-        impulseAudio,
-        bufferSize = 4096,
-        node = context.createScriptProcessor(bufferSize, 1, 1);
+    wetGain = context.createGain(),
+    dryGain = context.createGain(),
+    fileReader = new FileReader(),
+    audio;
 
     dryGain.gain.value = 1;
-    wetGain.gain.value = 1;
+    wetGain.gain.value = 0;
     wetGain.connect(context.destination);
     dryGain.connect(context.destination);
 
@@ -26,39 +20,15 @@ function readAudioFile(files) {
     }
 }
  function playAudioFile(file) {
-     mix(1);
      source = context.createBufferSource();
      context.decodeAudioData(file, function (buffer) {
          source.buffer = buffer;
          source.loop = true;
-         source.connect(effect);
-         effect.connect(wetGain);
+         source.connect(dryGain);
+         source.connect(reverb);
+         reverb.connect(wetGain);
      });
  }
-
-function readImpulseFile(files) {
-    fileReader.readAsArrayBuffer(files[0]);
-    fileReader.onload = function (e) {
-        loadImpulse(e.target.result);
-        impulseAudio = fileReader.result;
-        console.log(("Filename: '" + files[0].name + "'"), ( "(" + ((Math.floor(files[0].size / 1024 / 1024 * 100)) / 100) + " MB)" ));
-    }
-}
-function loadImpulse(file) {
-    impulseSource = context.createBufferSource();
-        context.decodeAudioData(file, function (buffer) {
-            impulseSource.buffer = buffer;
-            convolver.buffer = impulseSource.buffer;
-        },
-            function(e){"Error with decoding audio data" + e.err});
-    convolver.connect(wetGain);
-    console.log(convolver);
-    }
-
-var mix = function(value) {
-    dryGain.gain.value = ( 1.0 - value );
-    wetGain.gain.value = value;
-};
 
 window.onload = function() {
     document.getElementById('play').disabled=true;
@@ -67,12 +37,13 @@ window.onload = function() {
     document.getElementById('play').addEventListener('click', start_Audio);
     document.getElementById('stop').addEventListener('click', stop_Audio);
     document.getElementById('detune_Audio').addEventListener('input', function(){
-        source.detune.value = this.value;
-        document.querySelector('#detune_Audio_Value').value = this.value/100 + " Semitones";
+        source.detune.value = this.value*100;
+        document.querySelector('#detune_Audio_Value').value = this.value + " Semitones";
     });
-    document.getElementById('bitcrush_Amount').addEventListener('input', function(){
-        node.normfreq = this.value;
-        document.querySelector('#bitcrush_Amount_Value').value = this.value;
+    document.getElementById('reverb_Amount').addEventListener('input', function(){
+        dryGain.gain.value = 1 - this.value/100;
+        wetGain.gain.value = this.value/100;
+        document.querySelector('#reverb_Amount_Value').value = this.value + "%";
     });
 };
 function start_Audio() {
@@ -93,23 +64,15 @@ function stop_Audio() {
     document.getElementById('detune_Audio_Value').value = 0 + " Semitones";
 }
 
-effect = (function() {
-    node.bits = 16; // between 1 and 16
-    node.normfreq = 1; // between 0.0 and 1.0
-    var step = Math.pow(1/2, node.bits),
-        phaser = 0,
-        last = 0;
-    node.onaudioprocess = function(e) {
-        var input = e.inputBuffer.getChannelData(0),
-            output = e.outputBuffer.getChannelData(0);
-        for (var i = 0; i < bufferSize; i++) {
-            phaser += node.normfreq;
-            if (phaser >= 1.0) {
-                phaser -= 1.0;
-                last = step * Math.floor(input[i] / step + 0.5);
-            }
-            output[i] = last;
-        }
-    };
-    return node;
+var reverb = (function() {
+    var convolver = context.createConvolver(),
+        noiseBuffer = context.createBuffer(2, 0.5 * context.sampleRate, context.sampleRate),
+        left = noiseBuffer.getChannelData(0),
+        right = noiseBuffer.getChannelData(1);
+    for (var i = 0; i < noiseBuffer.length; i++) {
+        left[i] = Math.random() * 2 - 1;
+        right[i] = Math.random() * 2 - 1;
+    }
+    convolver.buffer = noiseBuffer;
+    return convolver;
 })();
